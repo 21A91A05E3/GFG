80034c14ae48fc29fc87ea2a6025d9f295c6dca9
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2024-02-10 19:00:28 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Number of paths in a matrix with k coins/number-of-paths-in-a-matrix-with-k-coins.cpp
@@ -0,0 +1,51 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+public:
+    long long noofpaths(int i,int j,int k,vector<vector<int>> &arr,vector<vector<vector<int>>>&dp)
+    {
+        if(i==0 and j==0 and k==arr[i][j])return 1;
+        
+        if(i<0 or j<0 or k<0)return 0;
+        
+        if(dp[i][j][k]!=-1)return dp[i][j][k];
+        
+         int p=0,np=0;
+        
+        p=noofpaths(i-1,j,k-arr[i][j],arr,dp);
+        
+        np=noofpaths(i,j-1,k-arr[i][j],arr,dp);
+        
+        
+        return dp[i][j][k]=p+np;
+        
+    }
+    long long numberOfPath(int n, int k, vector<vector<int>> &arr){
+        vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(n+1,vector<int>(k+1,-1)));
+        return noofpaths(n-1,n-1,k,arr,dp);
+        // Code Here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    Solution obj;
+    int i,j,k,l,m,n,t;
+    cin>>t;
+    while(t--)
+    {
+        cin>>k>>n;
+        vector<vector<int>> v(n, vector<int>(n, 0));
+        for(i=0;i<n;i++)
+            for(j=0;j<n;j++)
+                cin>>v[i][j];
+        cout << obj.numberOfPath(n, k, v) << endl;
+    }
+}
+// } Driver Code Ends
