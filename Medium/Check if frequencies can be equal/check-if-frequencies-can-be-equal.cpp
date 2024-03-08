e7592b863fd6f050ea53a9121e748c217f36943f
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2024-03-08 21:25:55 +0530

    Added Solution - GfG to GitHub

---
+++ Medium/Check if frequencies can be equal/check-if-frequencies-can-be-equal.cpp
@@ -0,0 +1,58 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+	bool sameFreq(string s)
+	{
+	    vector<int>frq(27,0);
+	    for(auto it:s){
+	        frq[it-'a']++;
+	    }
+	    unordered_map<int,int>mp;
+	    for(auto it:frq)
+	    {
+	        if(it) mp[it]++;
+	        if(mp.size() > 2) return false;
+	    }
+	  // for(auto it:mp)cout<<it.first<<" ---> "<<it.second<<" ";
+	   if(mp.size() == 1)return true;
+	   else{
+	       int me = 0 , se = 1000001;
+	       for(auto it: mp)
+	       {
+	            me = max(me,it.first);
+	            se = min(se, it.first);
+	       }
+	  // cout<<me<<" "<<se<<" ";
+	       if(mp[me]==1 and me-se == 1)return true;
+	       else if(mp[se] == 1 and se == 1)return true;
+	       
+	   }
+	  // cout<<"hi";
+	   return false;
+	    
+	  
+	}
+};
+
+//{ Driver Code Starts.
+int main(){
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+        Solution ob;
+        cout<<ob.sameFreq(s)<<endl;
+    }
+}
+
+
+
+// } Driver Code Ends
