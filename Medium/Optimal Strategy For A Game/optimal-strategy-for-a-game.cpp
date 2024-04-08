ba01968ad22d851588820ddc8d5834b31429fab3
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2024-04-08 22:47:25 +0530

    Updated Solution - GfG to GitHub

--- Medium/Optimal Strategy For A Game/optimal-strategy-for-a-game.cpp
+++ Medium/Optimal Strategy For A Game/optimal-strategy-for-a-game.cpp
@@ -7,18 +7,18 @@ using namespace std;
 //Function to find the maximum possible amount of money we can win.
 class Solution{
     public:
-    long long leela(int i,int j,int arr[], vector<vector<long long>> &dp)
+        long long pallavi(int i,int j,int arr[], vector<vector<long long>> &dp)
     {
         if(i>j) return 0;
         if(dp[i][j]!=-1) return dp[i][j];
-        long long t1=arr[i]+min(leela(i+2,j,arr,dp),leela(i+1,j-1,arr,dp));
-        long long t2=arr[j]+min(leela(i,j-2,arr,dp),leela(i+1,j-1,arr,dp));
+        long long t1=arr[i]+min(pallavi(i+2,j,arr,dp),pallavi(i+1,j-1,arr,dp));
+        long long t2=arr[j]+min(pallavi(i,j-2,arr,dp),pallavi(i+1,j-1,arr,dp));
         
         return dp[i][j]=max(t1,t2);
     }
     long long maximumAmount(int n, int arr[]){
         vector<vector<long long>> dp(n,vector<long long>(n,-1));
-        return leela(0,n-1,arr,dp);
+        return pallavi(0,n-1,arr,dp);
     }
 };
 
