420047f5caaa9a2bf757e243ad4e04309fb9e16b
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2024-04-24 22:14:27 +0530

    Updated Solution - GfG to GitHub

--- Medium/Paths to reach origin/paths-to-reach-origin.cpp
+++ Medium/Paths to reach origin/paths-to-reach-origin.cpp
@@ -10,7 +10,7 @@ using namespace std;
 class Solution
 {
 public:
-    int paths(int x,int y,vector<vector<int>>&dp){
+     int paths(int x,int y,vector<vector<int>>&dp){
         if(x == 0  and y == 0)return 1;
         if(x<0 || y<0)return 0;
         if(dp[x][y]!=-1)return dp[x][y];
