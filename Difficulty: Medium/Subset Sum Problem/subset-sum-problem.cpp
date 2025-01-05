d4384d8565289b312f02d3a4ee5900b3e9196258
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2025-01-05 22:58:50 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
+++ Difficulty: Medium/Subset Sum Problem/subset-sum-problem.cpp
@@ -12,23 +12,39 @@ class Solution {
     
     int fun(int i,vector<int>& arr, int target, vector<vector<int>>&dp){
         
-        if(i == arr.size()){
-            if(target == 0)return 1;
-            return 0;
+        if(target == 0) return  1;
+        
+        if(i == 0){
+            return arr[0] == target;
         }
         
         if(target < 0) return 0;
         
+        
         if(dp[i][target] != -1) return dp[i][target];
         
-        return dp[i][target] = fun(i+1,arr,target-arr[i],dp) |fun(i+1,arr,target,dp) ;
+        return dp[i][target] = fun(i-1,arr,target-arr[i],dp) |fun(i-1,arr,target,dp) ;
     }
     
     bool isSubsetSum(vector<int>& arr, int target) {
         // code here
-        vector<vector<int>>dp(arr.size(),vector<int>(target+1,-1));
+        // vector<vector<int>>dp(arr.size(),vector<int>(target+1,-1));
+        // return fun(arr.size()-1,arr,target,dp) == 1;
         
-        return fun(0,arr,target,dp) == 1;
+        int n = arr.size();
+        vector<vector<bool>>dp(n,vector<bool>(target+1,false));
+        for(int i = 0 ; i < n; i++) dp[i][0] = true;
+        dp[0][arr[0]] = true;
+        for(int  i = 1 ; i < n  ; i++){
+            for(int t = 1 ; t <= target ; t++){
+                
+                bool pick = false;
+                if(arr[i] <= t) pick = dp[i-1][t-arr[i]];
+                bool nonpick = dp[i-1][t];
+                dp[i][t] = pick | nonpick;
+            }
+        }
+        return dp[n-1][target];
     }
 };
 
