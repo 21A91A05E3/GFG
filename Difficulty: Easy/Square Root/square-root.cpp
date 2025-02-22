6a7f6fc461fe90eb54b9bf24eaba0d1a4efd8731
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2025-02-22 17:40:51 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Square Root/square-root.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// Function to find square root
+// x: element to find square root
+class Solution {
+  public:
+    int floorSqrt(int n) {
+        // Your code goes here
+        int low = 1;
+        int high = n;
+        int mid , ans;
+        while(low <= high){
+            mid = (low+high)/2;
+            if(mid*mid > n){
+                high = mid-1;
+            }
+            else{
+                ans = mid;
+                low = mid+1;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        Solution obj;
+        cout << obj.floorSqrt(n) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
