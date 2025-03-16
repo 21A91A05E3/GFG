129a6b260baae631de2215d6d3ff008501634fcf
Harika Kannuri <97424858+21A91A05E3@users.noreply.github.com>
2025-03-16 13:44:53 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
+++ Difficulty: Hard/Minimize Max Distance to Gas Station/minimize-max-distance-to-gas-station.cpp
@@ -7,42 +7,41 @@ using namespace std;
 
 class Solution {
   public:
-    int numberOfGasStationsRequired(double dist , vector<int> &arr){
-         int n = arr.size(); // size of the array
-    int cnt = 0;
-    for (int i = 1; i < n; i++) {
-        int numberInBetween = ((arr[i] - arr[i - 1]) / dist);
-        if ((arr[i] - arr[i - 1]) == (dist * numberInBetween)) {
-            numberInBetween--;
+    int findStationCnt(long double dist,vector<int>&stations){
+        
+        int totalStaCnt = 0;
+        for(int i = 1 ; i < stations.size() ; i++){
+            int diff = stations[i] - stations[i-1];
+            int curStaCnt = diff/dist;
+            
+            if(stations[i]-stations[i-1] == dist*curStaCnt) curStaCnt--;
+            
+            totalStaCnt+=curStaCnt;
         }
-        cnt += numberInBetween;
+        return totalStaCnt;
     }
-    return cnt;
-    }
-    double findSmallestMaxDist(vector<int> &arr, int k) {
+    double findSmallestMaxDist(vector<int> &stations, int k) {
         // Code here
-      int n = arr.size(); // size of the array
-    long double low = 0;
-    long double high = 0;
-
-    //Find the maximum distance:
-    for (int i = 0; i < n - 1; i++) {
-        high = max(high, (long double)(arr[i + 1] - arr[i]));
-    }
-
-    //Apply Binary search:
-    long double diff = 1e-6 ;
-    while (high - low > diff) {
-        long double mid = (low + high) / (2.0);
-        int cnt = numberOfGasStationsRequired(mid, arr);
-        if (cnt > k) {
-            low = mid;
+        long double  low = 0;
+        long double high = 0;
+        
+        for(int i = 1 ; i < stations.size() ; i++){
+            high = max(high,(long double)(stations[i]-stations[i-1]));
         }
-        else {
-            high = mid;
+        
+        
+        long double diff = 1e-6;
+        while(high - low > diff){
+            long double mid = (low+high)/2.0;
+            
+            int staCnt = findStationCnt(mid,stations);
+            if(staCnt > k){
+               
+                low = mid;
+            }
+            else high = mid;
         }
-    }
-    return high;
+        return high;
     }
 };
 
